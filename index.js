8035a91097e1a62f1866a4e14013f191782dd56d
Dmitriy <khaitdmitriy@gmail.com>
2022-03-06 21:12:34 -0500

    commit tree 4f07a0326e136d6c1b4475dc8201d42552a024f8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr,event){
+    let newArr = [];
+    for(let i =0; i < arr.length; i++){
+        newArr[i] = "Thank you, " + arr[i] +", for the wonderful " + event + " gift!";
+    }
+    return newArr;
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n);
+        n--;
+    }
+}
+
+console.log(countDown(10))
